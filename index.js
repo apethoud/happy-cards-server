2393edaa69f78cb3535b75d6ddca5bb9d33e450a
Andrew Pethoud <andrewpethoud@gmail.com>
2022-10-27 11:55:09 -0700

    add get users route

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const db = require("./queries");
+const PORT = process.env.LOCALHOSTPORT;
 
 app.use(bodyParser.json());
 app.use(
@@ -11,9 +15,11 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.json({ info: "Initial route working." });
+  res.json({ info: "Initial route working!" });
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+app.get("/users", db.getUsers);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`);
 });
