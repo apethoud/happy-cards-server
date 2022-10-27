973606121ed300028dec5f8ee37ae296e5f05889
Andrew Pethoud <andrewpethoud@gmail.com>
2022-10-27 12:22:38 -0700

    move routes to separate file

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const db = require("./queries");
 const PORT = process.env.LOCALHOSTPORT;
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
-app.get("/", (req, res) => {
-  res.json({ info: "Initial route working!" });
-});
-
-app.get("/users", db.getUsers);
-
+require("./startup/routes")(app);
 require("./startup/prod")(app);
 
 app.listen(PORT, () => {
